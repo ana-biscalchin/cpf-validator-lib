bbcbb50e3b83d454bc419c3eefc57df85415102e
ana-biscalchin <anacarolinabis@gmail.com>
2019-05-06 22:58:14 -0300

    eslint implementado, testes e funções ok

--- test/validator.spec.js
+++ test/validator.spec.js
@@ -2,19 +2,32 @@ const chai = require("chai");
 const expect = chai.expect;
 const validator = require("../lib/validator");
 
-
 describe("lib validator", () => {
-  describe('Receber, verificar e retornar string', () => {
+  describe("Receber, verificar e retornar string", () => {
     it("should to be a string", () => {
-      expect(validator.receiveCPF("00478457162")).to.be.a("string");
+      expect(validator.receiveCPF("25509631058")).to.be.a("string");
     }),
-    it("deve ter onze digitos", () => {
-      expect(validator.receiveCPF("00478457162")).to.have.lengthOf(11);
+    it("must be eleven digits", () => {
+      expect(validator.receiveCPF("25509631058")).to.have.lengthOf(11);
+    });
+  }),
+  describe("Casos que não devem passar", () => {
+    it("should to be string", () => {
+      expect(validator.receiveCPF(25509631058)).to.equal(false);
     })
-  });
+    it("more than eleven digits", () => {
+      expect(validator.receiveCPF("2550963105800")).to.equal(false);
+    });
+    it("não pode ter dígitos iguais", () => {
+      expect(validator.receiveCPF("000000000000")).to.equal(false);
+    });
+  }),
   describe("Deve validar conforme Receita Federal", () => {
-    it("Compara os dois últimos dígitos com os resultados do validador", () => {
-      expect(validator.validateCPF("00478457162")).to.equal(true);
+    it("Compares the last two digits with the validator results", () => {
+      expect(validator.validateCPF("25509631058")).to.equal(true);
+    });
+    it("Compares the last two digits with the validator results - false case", () => {
+      expect(validator.validateCPF("25509631059")).to.equal(false);
     });
   });
 });
